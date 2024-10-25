624dbf56f44f9ab61c7c62f21090bbd8a969fffe
Estefani <yuditheylb0@gmail.com>
2024-10-24 20:27:07 -0400

    feat: correct CRUD endpoints for SEO configuration

---
+++ src/data/interfaces/seo-configuration.ts
@@ -0,0 +1,10 @@
+export interface SeoConfiguration {
+    meta_title: string;
+    og_title: string;
+    og_url: string;
+    og_image?: string;
+    og_image_height?: number;
+    og_image_width?: number;
+    meta_description: string;
+    og_type: string;
+  }
