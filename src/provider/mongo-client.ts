32fde73fbdd53685a8a5624f83d51f4f4fc132b6
LuisPerez007 <lpz631744@gmail.com>
2024-11-22 21:57:58 -0400

    integrar multer

--- src/provider/mongo-client.ts
+++ src/provider/mongo-client.ts
@@ -2,25 +2,30 @@
 import { Db } from 'mongodb';
 import UserQueries from '../queries/user-queries';
 import RoleQueries from '../queries/role-queries';
+import VolumeQueries from '../queries/volume-queries';
 import { UserWrapper } from '../data/interfaces/user-wrapper';
 import { RoleWrapper } from '../data/interfaces/role-wrapper';
 import SeoQueries from '../queries/seo-queries';
 import { SEOWrapper } from '../data/interfaces/seo-wrapper';
+import { VolumeWrapper } from '../data/interfaces/volume-wrapper';
 
 interface QueryWrapper {
     userQueries: UserWrapper;
     roleQueries: RoleWrapper;
     seoQueries: SEOWrapper;
+    volumeQueries: VolumeWrapper;
 }
 
-const MongoClient = (db: Db): QueryWrapper  => {
+const MongoClient = (db: Db): QueryWrapper => {
     const userQueries = UserQueries(db);
     const roleQueries = RoleQueries(db);
     const seoQueries = SeoQueries(db);
+    const volumeQueries = VolumeQueries(db);
     return {
         userQueries,
         roleQueries,
-        seoQueries
+        seoQueries,
+        volumeQueries
     } as unknown as QueryWrapper;
 }
 export default MongoClient;
