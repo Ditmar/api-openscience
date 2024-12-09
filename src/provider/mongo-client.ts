52c12ed760b20a9edb3dea1a35ec85fdf77d8211
VictoriaCk55 <noemi.victoria.ck.vargas@gmail.com>
2024-12-08 22:32:55 -0400

    feature/BACKEND-14_Gestion de Usuarios

--- src/provider/mongo-client.ts
+++ src/provider/mongo-client.ts
@@ -8,11 +8,14 @@ import { RoleWrapper } from '../data/interfaces/role-wrapper';
 import { VolumeWrapper } from '../data/interfaces/volume-wrapper';
 import SeoQueries from '../queries/seo-queries';
 import { SEOWrapper } from '../data/interfaces/seo-wrapper';
+import UserManagementQueries from '../queries/userManagement-queries';
+import { UserWrapper as UserManagementWrapper } from '../data/interfaces/userManagement-wrapper';
 
 interface QueryWrapper {
     userQueries: UserWrapper;
     roleQueries: RoleWrapper;
     volumeQueries: VolumeWrapper;
+    userManagementQueries: UserManagementWrapper;
     seoQueries: SEOWrapper;
 }
 
@@ -20,11 +23,13 @@ const MongoClient = (db: Db): QueryWrapper => {
     const userQueries = UserQueries(db);
     const roleQueries = RoleQueries(db);
     const volumeQueries = VolumeQueries(db);
+    const userManagementQueries = UserManagementQueries(db);
     const seoQueries = SeoQueries(db);
     return {
         userQueries,
         roleQueries,
         volumeQueries,
+        userManagementQueries,
         seoQueries
     } as unknown as QueryWrapper;
 }
